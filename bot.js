7c63e5d83f81237424ead0c183543c632103efe5
제로ㅣBrazil <51540538+Zero-Brazil734@users.noreply.github.com>
2019-11-13 09:49:38 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,35 @@ const client = new dbcm.Client({
 
 client.registerCommands(require.resolve("./commands"), { createSample: false })
 
+client.database.connect("mongodb://localhost/example", { useFindAndModify: false })
+client.database.registerModels(require.resolve("./models"))
+
+client.on("ready", () => {
+    console.log(config.readyMsg)
+})
+
+client.on("message", async message => {
+    if(message.system || message.author.bot || message.channel.type === "dm") return
+
+    if(config.logMessages === true) console.log(`[MESSAGE] [Guild: ${message.guild.name}] [Channel: ${message.channel.name}] [Author: ${message.author.tag}] - ${message.content}`)
+
+    client.database.models.get("user").findById(message.author.id, (err, res) => {
+        if(err) throw new Error(err)
+
+        if(res)
+    })
+
+    if(!message.content.startsWith(config.prefix)) return
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase()
+
+    client.runCommand(command, message, args).catch(err => { throw new Error(err) })
+})
+
 client.login(config.token)
 
-//Wait a minute, I'll write this later ~~sorry!~~
+process.on("uncaughtException", error => {
+    console.error(error)
+})
+
